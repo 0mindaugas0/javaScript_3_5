c152b798552b619f43e475f37382fa228f61351c
0mindaugas0 <mindaugas.mizaras@gmail.com>
2022-10-11 08:56:22 +0300

    Sukurtas projektas

---
+++ script.js
@@ -0,0 +1,26 @@
+let a;
+a = prompt("Įveskite pirmąosios kraštinės ilgį");
+a = + a;
+
+let b;
+b = prompt("Įveskite antrosios kraštinės ilgį");
+b = + b;
+
+let c;
+c = prompt("Įveskite trečiosiso kraštinės ilgį");
+c = + c;
+
+let pusperimetris = pusperimetrioSkaiciavimas(a, b, c);
+pusperimetris = + pusperimetris;
+
+let plotas = plotoSkaiciavimas(a, b, c, pusperimetris);
+console.log("Trikampio plotas yra: " + plotas)
+
+
+function plotoSkaiciavimas (krastine1, krastine2, krastine3, pusperimetrioSkaiciavimas){
+    return Math.sqrt(pusperimetrioSkaiciavimas * (pusperimetrioSkaiciavimas - krastine1) * (pusperimetrioSkaiciavimas - krastine2) * (pusperimetrioSkaiciavimas * krastine3));
+}
+
+function pusperimetrioSkaiciavimas (krastine1, krastine2, krastine3){
+    return (krastine1 + krastine2 + krastine3) / 2;
+}
